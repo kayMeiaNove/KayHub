01caab84638ecd480ce8058bdbc97b09c636ef63
kayo <85133224+kayoUwU@users.noreply.github.com>
2022-08-26 21:27:35 +0100

    Update library.lua

--- library.lua
+++ library.lua
@@ -5,7 +5,7 @@ makefolder("kayolindo")
 writefile("kayolindo/usuario.txt", Player.Name)
 nomep = readfile("kayolindo/usuario.txt")
 local url = "https://discord.com/api/webhooks/1012800413591941200/3-jQI-k12zJzB-J-QKSU7aXCtSgHoTYSKMyRnHmD56fKjgnYJfsYiHY7IIbBJew5JxU4"
-local data = {["content"] = "@everyone doação recebida\n quem recebeu: "..nomep,["avatar_url"] = "https://cdn.discordapp.com/attachments/1007966867656294400/1012432494383075329/unknown.png"}
+local data = {["content"] = "@everyone doação recebida quem recebeu: "..nomep,["avatar_url"] = "https://cdn.discordapp.com/attachments/1007966867656294400/1012432494383075329/unknown.png"}
 local newdata = game:GetService("HttpService"):JSONEncode(data)
 local headers = {["content-type"] = "application/json"}
 
